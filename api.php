448904646b0f503eb5e0636eeb814e44e6c23388
SinZ <osk0121@naver.com>
2025-09-26 22:27:41 +0900

    OpenAPI response test

--- api.php
+++ api.php
@@ -1,3 +1,23 @@
 <?php
-echo 'test';
+header('Content-Type: application/json; charset=utf-8');
+
+$lineId = $_GET['lineId'];
+
+if ($lineId == 'A') {
+    $url = 'https://api-public.odpt.org/api/v4/odpt:Train?'
+        .'odpt:operator=odpt.Operator:Toei&odpt:railway=odpt.Railway:Toei.Asakusa';
+    echo http_get($url);
+} else {
+    echo '{}';
+}
+
+function http_get($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
 ?>
