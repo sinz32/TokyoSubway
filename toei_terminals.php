47eb0b8c693187d3290fe04629f2fa4a59c0a763
SinZ <osk0121@naver.com>
2025-10-10 17:49:11 +0900

    terminal list added

--- toei_terminals.php
+++ toei_terminals.php
@@ -6,7 +6,7 @@ $terminals = array(
     'NishiMagome' => '니시마고메',
     'Oshiage' => '오시아게',
     'Sengakuji' => '센카쿠지',
-    //케이세이 계통
+    //케이세이
     'Aoto' => '아오토',
     'ImbaNihonIdai' => '인바니혼이다이',
     'InzaiMakinohara' => '인자이마키노하라',
@@ -16,23 +16,47 @@ $terminals = array(
     'NaritaAirportTerminal1' => '나리타공항',
     'ShibayamaChiyoda' => '시바야마치요다',
     'Sogosando' => '소고산도',
-    //케이큐 계통
+    //케이큐
     'HanedaAirportTerminal1and2' => '하네다공항',
     'KanazawaBunko' => '카나자와분코',
     'KanagawaShimmachi' => '가나가와신마치',
     'KeikyuKurihama' => '케이큐쿠리하마',
     'Misakiguchi' => '미사키구치',
     'Miurakaigan' => '미우라카이간',
-    'Shinagawa' => '시나가와'
+    'Shinagawa' => '시나가와',
 
 
     //미타선
-
+    'NishiTakashimadaira' => '니시타카시마다이라',
+    'ShirokaneTakanawa' => '시로카네타카나와',
+    'Takashimadaira' => '타카시마다이라',
+    //도큐
+    'Hiyoshi' => '히요시',
+    'MusashiKosugi' => '무사시코스기',
+    //도큐 + 소테츠
+    'ShinYokohama' => '신요코하마',
+    //소테츠
+    'Ebina' => '에비나',
+    'Yamato' => '야마토',
+    'Nishiya' => '니시야',
+    'Shonandai' => '쇼난다이',
 
     //신주쿠선
-
+    'Motoyawata' => '모토야와타',
+    //케이오
+    'KeioTamaCenter' => '케이오타마센터',
+    'Sasazuka' => '사사즈카',
+    'Hashimoto' => '하시모토',
 
     //오에도선
+    'Tochomae' => '도초마에',
+    'Hikarigaoka' => '히카리가오카',
+    'Shinjuku' => '신주쿠',
+    'Shiodome' => '시오도메',
+    'Ojima' => '오지마',
+    'Wakabadai' => '와카다바이',
+    'ShinOkachimachi' => '신오카치마치'
+
 )
 
 ?>
