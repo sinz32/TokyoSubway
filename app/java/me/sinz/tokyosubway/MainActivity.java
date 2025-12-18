d932d154b07ee8cd78a8d53a31561e4975851d71
SinZ <osk0121@naver.com>
2025-12-18 23:36:35 +0900

    Hybrid app compatibility implemented

--- app/java/me/sinz/tokyosubway/MainActivity.java
+++ app/java/me/sinz/tokyosubway/MainActivity.java
@@ -2,6 +2,7 @@ package me.sinz.tokyosubway;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 
@@ -14,7 +15,9 @@ public class MainActivity extends Activity {
         layout.setOrientation(1);
         WebView web = new WebView(this);
         web.loadUrl("안알랴줌");
-        web.getSettings().setJavaScriptEnabled(true);
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setUserAgentString(settings.getUserAgentString()+" AndroidApp/1.0");
         web.setLayoutParams(new LinearLayout.LayoutParams(-1, -1));
         layout.addView(web);
         layout.setLayoutParams(new LinearLayout.LayoutParams(-1, -1));
